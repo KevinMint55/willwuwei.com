ddea7af0a046b2784830a75afcc34afad37ac04f
KevinMint55 <943642973@qq.com>
2019-04-24 23:10:50 +0800

    修改接口地址

--- api.config.js
+++ api.config.js
@@ -2,5 +2,5 @@ const isProdMode = Object.is(process.env.NODE_ENV, 'production');
 
 export default {
   cdnUrl: isProdMode ? 'https://qiniu.kevinmint.com/' : 'https://qiniu.kevinmint.com/',
-  baseURL: isProdMode ? 'http://111.231.249.239:7000/' : 'http://10.1.6.74:7000/',
+  baseURL: isProdMode ? 'https://api.kevinmint.com/' : 'http://10.1.6.74:7000/',
 };
